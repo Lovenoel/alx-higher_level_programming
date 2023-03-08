91e6b09d0119d1d6c2fb78629f2b6d94bc4c27f8
lovenoel <kinglovenoel@gmail.com>
2023-03-07 18:30:14 -0800

    task 2

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 2-print_alphabet.py
+
+"""print the alaphabet in lowercase, not followed by a newline."""
+for leterrs in range(97, 123):
+    print("{}".format(chr(letter)), end="")
