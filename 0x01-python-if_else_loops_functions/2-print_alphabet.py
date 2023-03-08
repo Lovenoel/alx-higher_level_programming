d30c84f4d3028860c733fd26690c76bacb3f7266
lovenoel <kinglovenoel@gmail.com>
2023-03-07 20:30:52 -0800

    task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # 2-print_alphabet.py
 
-leter = ASCII alphabet in lowercase
 for letters in range(97, 123):
-    print(letters)
+    print("{}".format(chr( letters)))
---
+++ 0x01-python-if_else_loops_functions/love.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+var = chr(97)
+print(var)
