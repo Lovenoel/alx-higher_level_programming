f17d5de530f21988acb6f08f2428148eda4a6b0f
lovenoel <kinglovenoel@gmail.com>
2023-03-07 20:09:12 -0800

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for xn in range(ord('a'), ord('z') + 1):
-    if chr(xn) != q and chr(xn) != e:
+    if chr(xn) != 'q' and chr(xn) != 'e':
         print("{}".format(chr(xn)), end="")
