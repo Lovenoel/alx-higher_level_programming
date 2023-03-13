70274c9c38aee1535d45fdad3dc8f8674d3d9fe8
lovenoel <kinglovenoel@gmail.com>
2023-03-13 16:30:24 -0700

    task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,25 +7,25 @@
  */
 int is_palindrome(listint_t **head)
 {
-        listint_t *current = *head;
-        int tmp[100], i = 0, s = 0;
+	listint_t *current = *head;
+	int tmp[100], i = 0, s = 0;
 
-        if (!*head || !head || !current->next)
-                return (1);
+	if (!*head || !head || !current->next)
+		return (1);
 
-        while (current)
-        {
-            tmp[i] = current->n;
-            i++;
-            current = current->next;
-        }
-        i--;
-        while (s <= i)
-        {
-            if(tmp[s] != tmp[i])
-                return (0);
-            s++;
-            i--;
-        }
-        return (1);
+	while (current)
+	{
+		tmp[i] = current->n;
+		i++;
+		current = current->next;
+	}
+	i--;
+	while (s <= i)
+	{
+		if (tmp[s] != tmp[i])
+			return (0);
+		s++;
+		i--;
+	}
+	return (1);
 }
