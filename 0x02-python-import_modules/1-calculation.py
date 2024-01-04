9dfbacafad808a59813a0ecd3c0574f57350a99c
lovenoel <kinglovenoel@gmail.com>
2024-01-04 14:01:39 -0800

    tasks

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+# Importing functions from calculator file
 from calculator_1 import add, sub, mul, div
+
+# Variables
 a = 10
 b = 5
+# Printing their various values
 print("{} + {} = {}".format(a, b, add(a, b)))
 print("{} - {} = {}".format(a, b, sub(a, b)))
 print("{} * {} = {}".format(a, b, mul(a, b)))
