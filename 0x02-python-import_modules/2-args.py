4a9ecbb764ec771ff2a9600600a20b554a2cc7f7
lovenoel <kinglovenoel@gmail.com>
2024-01-04 16:20:17 -0800

    tasks

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    # Importing the argv variable from the sys module
-    from sys import argv
-    # Getting the number of arguments
-    num_arguments = len(argv) - 1  # Subtracting 1 to exclude the script name
-    # Printing the number of arguments
-    if num_arguments == 0:
-        print("{} arguments.".format(num_arguments))
-    else:
-        print("{} arguments:".format(num_arguments))
-        # Printing each argument along with its position
-        for i in range(num_arguments):
-            print("{}: {}".format(i + 1, argv[i + 1]))
+# Importing the argv variable from the sys module
+from sys import argv
+# Getting the number of arguments
+num_arguments = len(argv) - 1  # Subtracting 1 to exclude the script name
+# Printing the number of arguments
+if num_arguments == 0:
+    print("{} arguments.".format(num_arguments))
+else:
+    print("{} arguments:".format(num_arguments))
+    # Printing each argument along with its position
+    for i in range(num_arguments):
+        print("{}: {}".format(i + 1, argv[i + 1]))
