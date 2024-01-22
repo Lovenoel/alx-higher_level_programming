ef0ed8acf12155662ae56f87a885d4211da3b036
lovenoel <kinglovenoel@gmail.com>
2024-01-22 13:33:10 -0800

    tasks

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,7 +3,7 @@
 def magic_calculation(a, b):
     result = 0
 
-    for i in range(1, 4):
+    for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
@@ -11,5 +11,6 @@ def magic_calculation(a, b):
             result += (a ** b) / i
         except Exception:
             result += b + a
+            break
 
     return result
