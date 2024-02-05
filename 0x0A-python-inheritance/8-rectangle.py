11aab39a399a26a1b488701a694132dee51ddd7b
lovenoel <kinglovenoel@gmail.com>
2024-02-05 13:10:56 -0800

    tasks

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,19 +1,22 @@
-#!/usr/bin/python3
-"""A module of a class Rectangle that inherits from BaseGeometry
+#!usr/bin/python3
+"""A module of class Rectangle that inherits from BaseGeometry
 (7-base_geometry.py).
 """
 
 
 class BaseGeometry:
     """
-    Empty class representing a base geometry.
+    Class representing a base geometry.
 
     Methods:
-    - area(self): Raises an Exception with the message "area() is not
-    implemented."
+    - area(self): Raises an Exception with the message "area() is
+    not implemented."
     - integer_validator(self, name, value): Validates the value as an integer.
     """
     def area(self):
+        """
+        Raises an Exception with the message "area() is not implemented."
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
@@ -43,8 +46,8 @@ class Rectangle(BaseGeometry):
     - __height: The height of the rectangle.
 
     Methods:
-    - area(self): Calculates and returns the area of the rectangle.
-    - __str__(self): Returns a string representation of the rectangle.
+    - __init__(self, width, height): Initializes a rectangle with
+    the given width and height.
     """
     def __init__(self, width, height):
         """
@@ -54,19 +57,10 @@ class Rectangle(BaseGeometry):
         - width: The width of the rectangle.
         - height: The height of the rectangle.
         """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    def area(self):
-        """
-        Calculates and returns the area of the rectangle.
-
-        Returns:
-        - The area of the rectangle.
-        """
-        return self.__width * self.__height
 
     def __str__(self):
         """
