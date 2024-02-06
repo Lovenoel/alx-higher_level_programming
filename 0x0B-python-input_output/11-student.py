3d6734e5c3efe7576c8b8e19a6c4bf77e77c902d
lovenoel <kinglovenoel@gmail.com>
2024-02-06 23:57:04 +0300

    tasks

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -19,8 +19,7 @@ class Student:
         """
         if attrs is None:
             return self.__dict__
-        return {attr: getattr(self, attr) \
-                for attr in attrs if hasattr(self, attr)}
+        return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
 
     def reload_from_json(self, json):
         """Replace all attributes of the Student instance"""
