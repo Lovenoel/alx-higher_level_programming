0c7ac08a4e8f004fdf4f261c15dc96b384046f43
lovenoel <kinglovenoel@gmail.com>
2024-03-14 16:57:23 -0700

    tasks

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,10 @@
-/*
-a script that prints the full description of the table first_table from the
-database hbtn_0c_0 in your MySQL server.
-*/
-USE hbtn_0c_0;
-SHOW CREATE TABLE first_table;
+-- Script to print the full description of the table first_tablefrom the database hbtn_0c_0
+
+-- Define the database name
+SET @database_name = 'hbtn_0c_0';
+
+-- Get the table information from INFORMATION_SCHEMA
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = @database_name
+AND TABLE_NAME = 'first_table';
