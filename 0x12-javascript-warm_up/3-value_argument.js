bd3b543a56325147f65d72bdb4cc8e14e7845d1e
lovenoel <kinglovenoel@gmail.com>
2024-04-08 18:55:09 -0700

    tasks

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
-if (process.argv[2]) {
-  console.log('No arguments');
+const arg = process.argv[2];
+
+if (!arg) {
+  console.log("No argument");
 } else {
-  console.log(process.argv[2]);
+  console.log(arg);
 }
