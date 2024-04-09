fe917ebce82e6499aa4ccd5ec4f77a145faefbce
lovenoel <kinglovenoel@gmail.com>
2024-04-08 17:40:52 -0700

    tasks

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,8 +2,8 @@
 const num = parseInt(process.argv[2]);
 if (!isNaN(num)) {
   for (let i = 0; i < num; i++) {
-    console.log("C is fun");
+    console.log('C is fun');
   }
 } else {
-  console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 }
