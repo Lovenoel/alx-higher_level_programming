e9a8ef0f0dd09cb78512a38a04efca9ab2caa83a
lovenoel <kinglovenoel@gmail.com>
2024-04-08 18:07:55 -0700

    tasks

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -4,5 +4,8 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.value = 89;
-console.log(myObject);
+const updatedObject = {
+  ...myObject,
+  value: 89
+};
+console.log(updatedObject);
