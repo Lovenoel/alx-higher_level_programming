4ed0b68cb21a9891572ce58178ca1daa716088aa
lovenoel <kinglovenoel@gmail.com>
2024-04-25 14:06:43 -0700

    tasks

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# a POST request with parameters to a URL
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1" | cat
-
+# Send a POST request to the provided URL with email and subject parameters, then display the response body
+curl -s -X POST "$1" -d "email=test@gmail.com" \
+	-d "subject=I will always be here for PLD"
