cb3848ee3f36af8900b7dfbb26caa7d043435306
lovenoel <kinglovenoel@gmail.com>
2024-04-25 14:00:19 -0700

    tasks

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# a request to 0.0.0.0:5000/catch_me
-curl -s -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
+# Send a POST request to 0.0.0.0:5000/catch_me
+curl -s -X PUT "http://0.0.0.0:5000/catch_me" -d "user_id=98" -H "Origin: School"
