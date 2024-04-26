cf6fc8fc30ae92be3796163ddb6bd2716fd92976
lovenoel <kinglovenoel@gmail.com>
2024-04-26 13:15:26 -0700

    tasks

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,10 +5,11 @@ with the email as a parameter, and finally displays the body of the response.
 import requests
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-data = {'email': email}
-response = requests.post(url, data=data)
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
 
-print(response.text)
+    print(response.text)
