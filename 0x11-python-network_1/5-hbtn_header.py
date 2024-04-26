7a29f845190a17dcc2906bf9527e44ee17e5c9a3
lovenoel <kinglovenoel@gmail.com>
2024-04-26 13:08:41 -0700

    task

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,7 +5,13 @@ variable X-Request-Id in the response header
 import requests
 import sys
 
-url = sys.argv[1]
-response = requests.get(url)
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-print(response.headers.get('X-Request-Id'))
+    response = requests.get(url)
+    x_request_id = response.headers.get('X-Request-Id')
+
+    if x_request_id:
+        print(x_request_id)
+    else:
+        print("X-Request-Id header not found in the response.")
