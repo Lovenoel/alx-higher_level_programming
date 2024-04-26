bbf57818014bef3479ad214b0e1d908d91f4de38
lovenoel <kinglovenoel@gmail.com>
2024-04-26 13:21:11 -0700

    tasks

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,17 +5,20 @@ http://0.0.0.0:5000/search_user with the letter as a parameter.
 import requests
 import sys
 
-url = 'http://0.0.0.0:5000/search_user'
-q = sys.argv[1] if len(sys.argv) > 1 else ""
-
-data = {'q': q}
-response = requests.post(url, data=data)
-
-try:
-    json_response = response.json()
-    if json_response:
-        print("[{}] {}".format(json_response['id'], json_response['name']))
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
     else:
-        print("No result")
-except ValueError:
-    print("Not a valid JSON")
+        q = ""
+
+    payload = {'q': q}
+    response = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+
+    try:
+        json_data = response.json()
+        if json_data:
+            print("[{}] {}".format(json_data.get('id'), json_data.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
