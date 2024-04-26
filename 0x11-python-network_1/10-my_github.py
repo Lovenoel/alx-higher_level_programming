30edfcb9de107f28e4631990c83d62cca9421062
lovenoel <kinglovenoel@gmail.com>
2024-04-26 13:29:22 -0700

    tasks

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,10 +5,16 @@ API to display your id
 import requests
 import sys
 
-username = sys.argv[1]
-password = sys.argv[2]
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
 
-url = 'https://api.github.com/user'
-response = requests.get(url, auth=(username, password))
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(username, password))
 
-print(response.json().get('id', None))
+    if response.status_code == 200:
+        user_data = response.json()
+        user_id = user_data.get('id')
+        print(user_id)
+    else:
+        print("None")
