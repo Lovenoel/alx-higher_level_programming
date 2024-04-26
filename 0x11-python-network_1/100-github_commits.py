60bee8d1102b8abd49fd638833f4ae7c4c3830f9
lovenoel <kinglovenoel@gmail.com>
2024-04-26 13:52:25 -0700

    tasks

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,15 +5,19 @@ for a back-end position with multiple technical challenges
 import requests
 import sys
 
-repo_name = sys.argv[1]
-owner_name = sys.argv[2]
 
-url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
-response = requests.get(url)
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
 
-commits = response.json()
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+    response = requests.get(url)
 
-for commit in commits[:10]:
-    sha = commit['sha']
-    author_name = commit['commit']['author']['name']
-    print(f"{sha}: {author_name}")
+    if response.status_code == 200:
+        commits = response.json()[:10]
+        for commit in commits:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f"{sha}: {author_name}")
+        else:
+            print("Failed to fetch commits.Status code:", response.status_code)
