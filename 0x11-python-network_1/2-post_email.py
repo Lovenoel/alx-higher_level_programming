22ad87c6e6ffd33e8ee34e7c48f82c94015106fb
lovenoel <kinglovenoel@gmail.com>
2024-04-26 13:02:47 -0700

    tasks

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,15 +3,19 @@
 the email as a parameter, and displays the body of the response
 (decoded in utf-8)
 """
-import urllib.parse
 import urllib.request
+import urllib.parse
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-req = urllib.request.Request(url, data=data)
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('utf-8')  # Data should be bytes
 
-with urllib.request.urlopen(req) as response:
-    print(response.read().decode('utf-8'))
+    req = urllib.request.Request(url, data=data, method='POST')
+
+    with urllib.request.urlopen(req) as response:
+        body = response.read().decode('utf-8')
+        print(body)
